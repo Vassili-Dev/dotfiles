d4304e67120b980f2f74f8bc1e3218e95c438f47
VassiliDev <NickolaiVassiliev@gmail.com>
2023-12-04 10:14:16 -0500

    Fish init script program checking

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-  starship init fish | source
-  pyenv init - | source
+  if type -q starship
+    starship init fish| source
+  end
+  if type -q pyenv
+    pyenv init - | source
+  end
   set -gx MOZ_ENABLE_WAYLAND 1
 end
